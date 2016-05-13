46c66ee22330be452ba7c2590ac0c92c9c16b570
Patrick Logan <jpatricklogan@gmail.com>
2016-05-13 10:56:24 -0600

    added some gems and .gitignore
    Added Pry and Babbler, and "invented" .gitignore
     Please enter the commit message for your changes. Lines starting

--- ruby_classes_modules_examples.rb
+++ ruby_classes_modules_examples.rb
@@ -1,3 +1,22 @@
+# puts 'YAY Friday!'
+
+# def keep_coding
+#   puts 'Pop question: Are you going to keep coding over the weekend? (yes / no)'
+#   input = gets.strip.downcase
+#   if input == 'quit': exit: input = input.to_i'
+#   	input == 'yes'
+#     puts 'Good answer!'
+#   else
+#     puts 'You serious, bro? Do you even code?'
+#     keep_coding
+#   end
+# end
+
+# keep_coding
+
+require 'pry'
+require 'babbler'
+
 puts 'YAY Friday!'
 
 def keep_coding
@@ -6,9 +25,11 @@ def keep_coding
   if input == 'yes'
     puts 'Good answer!'
   else
-    puts 'You serious, bro? Do you even code?'
+    #puts 'You serious, bro? Do you even code?'
+    puts Babbler.babble
     keep_coding
   end
 end
 
 keep_coding
+
